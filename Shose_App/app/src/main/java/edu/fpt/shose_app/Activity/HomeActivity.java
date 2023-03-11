667e88dd342b98e600e7c3fe7ff222a695c52cf0
trungnv1 <tn01224092002@gmail.com>
2023-03-11 13:12:18 +0700

    11/03 comit login gmail, bottom nav home

---
+++ Shose_App/app/src/main/java/edu/fpt/shose_app/Activity/HomeActivity.java
@@ -0,0 +1,68 @@
+package edu.fpt.shose_app.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import edu.fpt.shose_app.R;
+
+
+public class HomeActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    FloatingActionButton floatingActionButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        initUi();
+        initAction();
+    }
+
+    private void initAction() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        // Handle home button click
+                        Log.d("TAG", "onNavigationItemSelected: Home");
+                        return true;
+                    case R.id.Favorite:
+                        // Handle profile button click
+                        Log.d("TAG", "onNavigationItemSelected: Favorite");
+                        return true;
+                    case R.id.Notifications:
+                        // Handle settings button click
+                        Log.d("TAG", "onNavigationItemSelected: Notifications");
+                        return true;
+                    case R.id.Profile:
+                        // Handle settings button click
+                        Log.d("TAG", "onNavigationItemSelected: Profile");
+                        return true;
+                }
+                return false;
+            }
+        });
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(HomeActivity.this, MyCartActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    private void initUi() {
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
+        floatingActionButton = findViewById(R.id.fab_home);
+    }
+}
---
+++ Shose_App/app/src/main/java/edu/fpt/shose_app/Model/User.java
@@ -0,0 +1,4 @@
+package edu.fpt.shose_app.Model;
+
+public class User {
+}
