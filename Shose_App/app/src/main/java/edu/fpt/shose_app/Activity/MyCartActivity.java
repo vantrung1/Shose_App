09b520a0c3691feda73d7cd07d83bd30d3ac7f62
daoTuan <minhtuan080722@gmail.com>
2023-03-29 16:28:05 +0700

    update mycart

--- Shose_App/app/src/main/java/edu/fpt/shose_app/Activity/MyCartActivity.java
+++ Shose_App/app/src/main/java/edu/fpt/shose_app/Activity/MyCartActivity.java
@@ -10,20 +10,24 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 import edu.fpt.shose_app.Adapter.MyCartAdapter;
 import edu.fpt.shose_app.Model.Cart;
+import edu.fpt.shose_app.EventBus.TotalEvent;
 import edu.fpt.shose_app.R;
+import edu.fpt.shose_app.Utils.Utils;
 
 public class MyCartActivity extends AppCompatActivity {
     Toolbar toolbar;
     RecyclerView recyclerViewCart;
     AppCompatButton appCompatButton;
-    TextView txt_subtotal, txt_shopping, txt_total;
-    ArrayList<Cart> arrayListCart;
+    TextView txt_total, textToast;
     MyCartAdapter adapter;
 
     @Override
@@ -31,6 +35,7 @@ public class MyCartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_cart);
         initUi();
+        totalCartCost();
     }
 
     private void initUi() {
@@ -50,20 +55,50 @@ public class MyCartActivity extends AppCompatActivity {
         LinearLayoutManager layoutManager = new LinearLayoutManager(MyCartActivity.this, LinearLayoutManager.VERTICAL, false);
         recyclerViewCart.setLayoutManager(layoutManager);
 
-        txt_subtotal = findViewById(R.id.txt_price_subtotal);
-        txt_shopping = findViewById(R.id.txt_price_shopping);
         txt_total = findViewById(R.id.txt_price_total);
 
-        arrayListCart = new ArrayList<>();
-        arrayListCart.add(new Cart( "https://thumbs.dreamstime.com/b/blue-shoes-29507491.jpg", "Nike", 20000, 1,40));
-        arrayListCart.add(new Cart( "https://thumbs.dreamstime.com/b/blue-shoes-29507491.jpg", "Nike", 20000, 1,39));
-        arrayListCart.add(new Cart( "https://thumbs.dreamstime.com/b/blue-shoes-29507491.jpg", "Nike", 20000, 1,42));
-        arrayListCart.add(new Cart( "https://thumbs.dreamstime.com/b/blue-shoes-29507491.jpg", "Nike", 20000, 1,42));
-        arrayListCart.add(new Cart( "https://thumbs.dreamstime.com/b/blue-shoes-29507491.jpg", "Nike", 20000, 1,42));
-        arrayListCart.add(new Cart( "https://thumbs.dreamstime.com/b/blue-shoes-29507491.jpg", "Nike", 20000, 1,42));
-        arrayListCart.add(new Cart( "https://thumbs.dreamstime.com/b/blue-shoes-29507491.jpg", "Nike", 20000, 1,42));
+        Utils.cartLists = new ArrayList<>();
+        Utils.cartLists.add(new Cart("https://thumbs.dreamstime.com/b/blue-shoes-29507491.jpg", "Nike", 20000, 1, 40));
+        Utils.cartLists.add(new Cart( "https://thumbs.dreamstime.com/b/blue-shoes-29507491.jpg", "Nike", 20000, 1,39));
+        Utils.cartLists.add(new Cart("https://thumbs.dreamstime.com/b/blue-shoes-29507491.jpg", "Nike", 20000, 1,42));
+        Utils.cartLists.add(new Cart("https://thumbs.dreamstime.com/b/blue-shoes-29507491.jpg", "Nike", 20000, 1, 40));
+        Utils.cartLists.add(new Cart( "https://thumbs.dreamstime.com/b/blue-shoes-29507491.jpg", "Nike", 20000, 1,39));
+        Utils.cartLists.add(new Cart("https://thumbs.dreamstime.com/b/blue-shoes-29507491.jpg", "Nike", 20000, 1,42));
+        Utils.cartLists.add(new Cart("https://thumbs.dreamstime.com/b/blue-shoes-29507491.jpg", "Nike", 20000, 1, 40));
+        if (Utils.cartLists.size() == 0) {
+            textToast.setText("Giỏ hàng trống");
+            textToast.setVisibility(View.VISIBLE);
+        } else {
+            adapter = new MyCartAdapter(getApplicationContext(), Utils.cartLists);
+            recyclerViewCart.setAdapter(adapter);
+        }
+    }
 
-        adapter = new MyCartAdapter(getApplicationContext(), arrayListCart);
-        recyclerViewCart.setAdapter(adapter);
+    private void totalCartCost() {
+        long total = 0;
+        for (int i = 0; i < Utils.cartLists.size(); i++) {
+            total = total + (Utils.cartLists.get(i).getPrice() * Utils.cartLists.get(i).getQuantity());
+        }
+        DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
+        txt_total.setText(decimalFormat.format(total));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onStop() {
+        EventBus.getDefault().unregister(this);
+        super.onStop();
+    }
+
+    @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
+    public void eventToatl(TotalEvent event) {
+        if (event != null) {
+            totalCartCost();
+        }
     }
 }
--- Shose_App/app/src/main/java/edu/fpt/shose_app/Adapter/MyCartAdapter.java
+++ Shose_App/app/src/main/java/edu/fpt/shose_app/Adapter/MyCartAdapter.java
@@ -1,6 +1,8 @@
 package edu.fpt.shose_app.Adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +14,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 
+import org.greenrobot.eventbus.EventBus;
+
 import java.util.List;
 
+import edu.fpt.shose_app.EventBus.TotalEvent;
 import edu.fpt.shose_app.Model.Cart;
 import edu.fpt.shose_app.R;
 
@@ -42,6 +47,43 @@ public class MyCartAdapter extends RecyclerView.Adapter<MyCartAdapter.myViewHold
         myViewHolder.txt_quantity_cart.setText(cart.getQuantity()+"");
         myViewHolder.txt_size_cart.setText(cart.getSize()+"");
         Glide.with(context).load(cart.getImage()).into(holder.img_cart);
+        holder.setListenner(new ImageClickListenner() {
+            @Override
+            public void onImageClick(View view, int pos, int giatri) {
+                if (giatri == 1) {
+                    if (cartList.get(pos).getQuantity() > 1) {
+                        int quantityNew = cartList.get(pos).getQuantity() - 1;
+                        cartList.get(pos).setQuantity(quantityNew);
+                    } else if (cartList.get(pos).getQuantity() == 1) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(view.getRootView().getContext());
+                        builder.setTitle("Thong bao");
+                        builder.setMessage("ban co muon xoa san pham nay khoi gio hang?");
+                        builder.setPositiveButton("Dong y", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                cartList.remove(pos);
+                                notifyDataSetChanged();
+                            }
+                        });
+                        builder.setNegativeButton("Huy", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        });
+                        builder.show();
+                    }
+                }
+                if (giatri == 2) {
+                    if (cartList.get(pos).getQuantity() >= 0) {
+                        int quantityNew = cartList.get(pos).getQuantity() + 1;
+                        cartList.get(pos).setQuantity(quantityNew);
+                    }
+                }
+                holder.txt_quantity_cart.setText(cartList.get(pos).getQuantity() + "");
+                EventBus.getDefault().postSticky(new TotalEvent());
+            }
+        });
     }
 
     @Override
@@ -49,12 +91,14 @@ public class MyCartAdapter extends RecyclerView.Adapter<MyCartAdapter.myViewHold
         return cartList.size();
     }
 
-    public class myViewHolder extends RecyclerView.ViewHolder {
+    public class myViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         ImageView img_cart, img_plus_cart, img_minus_cart, delete_cart;
         TextView txt_name_cart, txt_price_cart, txt_quantity_cart, txt_size_cart;
+        ImageClickListenner listenner;
 
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
+
             img_cart = itemView.findViewById(R.id.item_img_cart);
             img_plus_cart = itemView.findViewById(R.id.item_btnIMG_plus_cart);
             img_minus_cart = itemView.findViewById(R.id.item_btnIMG_minus_cart);
@@ -64,7 +108,26 @@ public class MyCartAdapter extends RecyclerView.Adapter<MyCartAdapter.myViewHold
             txt_price_cart = itemView.findViewById(R.id.item_txt_price_cart);
             txt_quantity_cart = itemView.findViewById(R.id.item_txt_quantity_cart);
             txt_size_cart = itemView.findViewById(R.id.item_txt_size_cart);
+
+            img_minus_cart.setOnClickListener(this);
+            img_plus_cart.setOnClickListener(this);
         }
 
+        public void setListenner(ImageClickListenner listenner) {
+            this.listenner = listenner;
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (view == img_minus_cart) {
+                listenner.onImageClick(view, getAdapterPosition(), 1);
+            } else if (view == img_plus_cart) {
+                listenner.onImageClick(view, getAdapterPosition(), 2);
+            }
+        }
+    }
+
+    public interface ImageClickListenner {
+        void onImageClick(View view, int pos, int giatri);
     }
 }
