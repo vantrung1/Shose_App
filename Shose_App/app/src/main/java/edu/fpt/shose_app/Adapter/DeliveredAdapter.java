68467b540f36eeb46099edd41e877d058a419272
daoTuan <minhtuan080722@gmail.com>
2023-05-02 21:40:46 +0700

    commit_UI_feedback

--- Shose_App/app/src/main/java/edu/fpt/shose_app/Adapter/DeliveredAdapter.java
+++ Shose_App/app/src/main/java/edu/fpt/shose_app/Adapter/DeliveredAdapter.java
@@ -1,6 +1,7 @@
 package edu.fpt.shose_app.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +19,8 @@ import com.bumptech.glide.Glide;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
+import edu.fpt.shose_app.Activity.Cancel_detail_activity;
+import edu.fpt.shose_app.Activity.EvaluateActivity;
 import edu.fpt.shose_app.Model.Oder;
 import edu.fpt.shose_app.R;
 
@@ -49,6 +52,17 @@ public class DeliveredAdapter extends RecyclerView.Adapter<DeliveredAdapter.myvi
     public void onBindViewHolder(@NonNull DeliveredAdapter.myviewHolder holder, int position) {
         holder.txt_quantity2.setText(oderArrayList.get(position).getQuantity() + "sản phẩm");
         holder.txtTotal.setText(oderArrayList.get(position).getTotal());
+        holder.setListenner(new ImageClickListenner() {
+            @Override
+            public void onImageClick(View view, int pos, int giatri) {
+                if (giatri == 1) {
+                    Intent intent = new Intent(context, EvaluateActivity.class);
+                    intent.putExtra("evaluate", oderArrayList.get(pos));
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(intent);
+                }
+            }
+        });
     }
 
     @Override
@@ -56,9 +70,10 @@ public class DeliveredAdapter extends RecyclerView.Adapter<DeliveredAdapter.myvi
         return oderArrayList.size();
     }
 
-    public class myviewHolder extends RecyclerView.ViewHolder {
-        TextView  txt_quantity2, txtTotal, txtShow;
+    public class myviewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView txt_quantity2, txtTotal, txtShow;
         AppCompatButton appCompatButton;
+        ImageClickListenner listenner;
 
         public myviewHolder(@NonNull View itemView) {
             super(itemView);
@@ -70,6 +85,22 @@ public class DeliveredAdapter extends RecyclerView.Adapter<DeliveredAdapter.myvi
             recyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false));
             products_oder_adapter = new Products_Oder_Adapter(context, oderArrayList.get(0).getProducts());
             recyclerView.setAdapter(products_oder_adapter);
+            appCompatButton.setOnClickListener(this);
         }
+
+        public void setListenner(DeliveredAdapter.ImageClickListenner listenner) {
+            this.listenner = listenner;
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (view == appCompatButton) {
+                listenner.onImageClick(view, getAdapterPosition(), 1);
+            }
+        }
+    }
+
+    public interface ImageClickListenner {
+        void onImageClick(View view, int pos, int giatri);
     }
 }
